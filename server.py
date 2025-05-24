f05379b4079015c7efdea34522e3c02db484a9e7
hoangpham96 <hoangpham96@gmail.com>
2025-05-24 16:00:47 +1000

    Add functionality to list content if it's a directory

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ class Case_directory_no_index_file(Case):
                not os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        pass
+        handler.list_dir(handler.full_path)
 
 class requestHandler(httpServer.BaseHTTPRequestHandler):
     Page = '''\
@@ -83,9 +83,20 @@ class requestHandler(httpServer.BaseHTTPRequestHandler):
         </html>
         """
     
+    Listing_Page = '''\
+        <html>
+        <body>
+        <ul>
+        {0}
+        </ul>
+        </body>
+        </html>
+        '''
+    
     Cases = [Case_no_file(),
              Case_existing_file(),
              Case_directory_index_file(),
+             Case_directory_no_index_file(),
              Case_always_fail()]
 
     def do_GET(self):
@@ -115,7 +126,7 @@ class requestHandler(httpServer.BaseHTTPRequestHandler):
     def send_content(self, content, status=200):
         if isinstance(content, str):
             content = content.encode('utf-8')
-            
+
         self.send_response(status)
         self.send_header("Content-type", "text/html")
         self.send_header("Content-Length", str(len(content)))
@@ -129,6 +140,17 @@ class requestHandler(httpServer.BaseHTTPRequestHandler):
             self.send_content(content)
         except IOError as msg:
             msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) 
+                for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
             self.handle_error(msg)
 
     # def create_page(self):
